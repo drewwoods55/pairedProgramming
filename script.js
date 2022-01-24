41b933cf43cc5c1dc376e7acdc24ab17d4c8e1a4
Daniel Escamilla <descamilla128@gmail.com>
2022-01-24 16:05:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Write a JavaScript function that takes in a random array and outputs two new arrays of odd numbers and even numbers.
+// Example:
+// Input: [2, 4, 7, 11, 15, 16]
+
+// Output: Even numbers: [2, 4, 16]
+// 	  Odd Numbers: [7, 11, 15
+
+let random = [2, 4, 7, 11, 15, 16]
+
+function seperateNum(number) {
+    let odd = number % 2 === 0;
+}
+
+console.log(seperateNum)
+
+// Create a function that checks an array for prime numbers then inserts any pimes into a new array.                               
+// Example 1:                                                                                                                                       Input: numArray = [1,2,3,4,5,6,7,8,9,10]
+// Output: primeArray = [2,3,5,7]
+// Example 2:												Input: numArray = [10, 18, 19, 29, 33, 35, 47, 66, 83] 
+// Output: primeArray = [19 ,29 ,47 , 83]
