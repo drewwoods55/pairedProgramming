f6214be3e53041d5830e8c845d02989421d9f46f
Andrew  Woods <andrewwoods@Andrews-MacBook-Pro.local>
2022-01-25 10:56:56 -0500

    messing with challenges

--- script.js
+++ script.js
@@ -5,16 +5,42 @@
 // Output: Even numbers: [2, 4, 16]
 // 	  Odd Numbers: [7, 11, 15
 
-let random = [2, 4, 7, 11, 15, 16]
+// let random = [2, 4, 7, 11, 15, 16]
 
-function seperateNum(number) {
-    let odd = number % 2 === 0;
-}
+// function seperateNum(number) {
+//     let odd = number % 2 === 0;
+// }
 
-console.log(seperateNum)
+// console.log(seperateNum)
+
+const random = [2, 4, 7, 11, 15, 16];
+
+const even = random.filter(function(separateNum) {
+    return separateNum % 2 === 0;
+})
+const odd = random.filter(function(separateNum) {
+    return separateNum % 2 === 1;
+})
+
+console.log(even)
+console.log(odd)
 
 // Create a function that checks an array for prime numbers then inserts any pimes into a new array.                               
-// Example 1:                                                                                                                                       Input: numArray = [1,2,3,4,5,6,7,8,9,10]
+// Example 1: 
+// Input: umArray = n[1,2,3,4,5,6,7,8,9,10]
 // Output: primeArray = [2,3,5,7]
-// Example 2:												Input: numArray = [10, 18, 19, 29, 33, 35, 47, 66, 83] 
+// Example 2:		
+// Input: numArray = [10, 18, 19, 29, 33, 35, 47, 66, 83] 
 // Output: primeArray = [19 ,29 ,47 , 83]
+
+const NumArray = [1,2,3,4,5,6,7,8,9,10]
+
+function checkPrime(num){
+    for(i=2; i <= num/2; i++){
+        if (num[i]% i==0){
+            console.log('prime numbers')
+        }
+    }
+
+}
+console.log(checkPrime[NumArray])
