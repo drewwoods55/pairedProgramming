8a56f28fd5e31000aefc3e0ef0a9682fea025ebd
Daniel Escamilla <descamilla128@gmail.com>
2022-01-26 10:47:16 -0500

    daniels turn

--- script.js
+++ script.js
@@ -10,7 +10,6 @@
 // function seperateNum(number) {
 //     let odd = number % 2 === 0;
 // }
-<<<<<<< HEAD
 
 // console.log(seperateNum)
 
@@ -23,46 +22,68 @@ const odd = random.filter(function(separateNum) {
     return separateNum % 2 === 1;
 })
 
-=======
-
-// console.log(seperateNum)
-
-const random = [2, 4, 7, 11, 15, 16];
-
-const even = random.filter(function(separateNum) {
-    return separateNum % 2 === 0;
-})
-const odd = random.filter(function(separateNum) {
-    return separateNum % 2 === 1;
-})
-
->>>>>>> c5dd8243a808e4228022f6a2e8d47432ad7725e7
 console.log(even)
 console.log(odd)
 
 // Create a function that checks an array for prime numbers then inserts any pimes into a new array.                               
 // Example 1: 
-<<<<<<< HEAD
 // Input: umArray = n[1,2,3,4,5,6,7,8,9,10]
-=======
-// Input: numArray = [1,2,3,4,5,6,7,8,9,10]
->>>>>>> c5dd8243a808e4228022f6a2e8d47432ad7725e7
 // Output: primeArray = [2,3,5,7]
 // Example 2:		
 // Input: numArray = [10, 18, 19, 29, 33, 35, 47, 66, 83] 
 // Output: primeArray = [19 ,29 ,47 , 83]
 
-<<<<<<< HEAD
 const NumArray = [1,2,3,4,5,6,7,8,9,10]
 
 function checkPrime(num){
-    for(i=2; i <= num/2; i++){
+    for(i=2; i<NumArray.length; i++){
         if (num[i]% i==0){
             console.log('prime numbers')
         }
     }
 
 }
-console.log(checkPrime[NumArray])
-=======
->>>>>>> c5dd8243a808e4228022f6a2e8d47432ad7725e7
+checkPrime(6)
+
+
+const numArray = [1,2,3,4,5,6,7,8,9,10]
+function checkArray(num){
+    for(i=2; i < numArray.length; i++){
+        if(num[i] % 2 === 0){
+            console.log("prime numbers")
+        }
+    }
+}
+
+
+checkArray(numArray)
+
+// Create a Function Called “vowelChecker” that takes in a single argument (x) and logs weather or not the input is a vowel
+// Example 1:
+// Input: vowelChecker(‘b’)
+// Output: ‘This is not a vowel’
+// Example 2:
+// Input: vowelChecker(‘a’)
+// Output: ‘This is a vowel’
+
+//function vowelChecker(a)
+
+// function isVowel(char)
+// {
+//   if (char.length == 1)
+//   {
+//     let vowels = "aeiou";
+//     let isVowel = vowels.indexOf(char) >= 0 ? true : false;
+
+//     return isVowel;
+//   }
+// }
+
+function vowelChecker(x){
+    if(x=='a'&& 'e'&& 'i'&&'o'&&'u'){
+        console.log('this is a vowel')
+    }else{console.log('this is not a vowel')}
+
+
+}
+console.log(vowelChecker('A'))
